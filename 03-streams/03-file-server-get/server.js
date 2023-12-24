1f1a8ec25c2e636b97a0e5672774c747382c6a2d
zalazaeva <szalazaeva@gmail.com>
2023-12-24 21:07:31 +0500

    03/03 done

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,20 +1,39 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.indexOf('/') !== -1) {
+    res.statusCode = 400;
+    res.end();
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
+
+  const stream = fs.createReadStream(filepath);
 
   switch (req.method) {
     case 'GET':
-
+      stream.on('data', (chunk) => {
+        res.write(chunk)
+        res.end();
+      })
+      stream.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end();
+        } else {
+          res.statusCode = 500;
+          res.end();
+        }
+      })
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');

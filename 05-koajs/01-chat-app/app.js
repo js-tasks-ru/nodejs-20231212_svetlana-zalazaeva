823771391de94a19ab2091e933920b0ba9dd6058
zalazaeva <szalazaeva@gmail.com>
2024-01-19 00:17:40 +0500

    05 module is done

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,17 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+var message = null;
+
 router.get('/subscribe', async (ctx, next) => {
+  await new Promise(resolve => setTimeout(resolve, 5000))
+  ctx.body = message;
+  message = null;
 });
 
 router.post('/publish', async (ctx, next) => {
+  message = ctx.request.body.message
+  ctx.response.status = 200;
 });
 
 app.use(router.routes());

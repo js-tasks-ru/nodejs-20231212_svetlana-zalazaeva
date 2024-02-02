010c35b1382ca038ef2e3e1ffa155a0d43c35ef9
zalazaeva <szalazaeva@gmail.com>
2024-02-02 23:38:40 +0500

    07 module 1 task done

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,17 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      const user = await User.findOne({ email })
+      if (!user) {
+        done(null, false, 'Нет такого пользователя')
+      }
+      const isValid = await user.checkPassword(password);
+      if (!isValid) {
+        done(null, false, 'Неверный пароль')
+      }
+      done(null, user);
     },
 );

488242181499a31b1d76b5c3f558a5fc7cb32195
zalazaeva <szalazaeva@gmail.com>
2024-02-22 00:07:09 +0500

    09 module is done

--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -4,16 +4,35 @@ const Session = require('./models/Session');
 const Message = require('./models/Message');
 
 function socket(server) {
-  const io = socketIO(server);
+  const io = socketIO(server, {
+    allowEIO3: true // false by default
+  });
 
-  io.use(async function(socket, next) {
+  io.use(async (socket, next) => {
+    const token = socket.handshake.query.token;
+    if (!token)  {
+      next(new Error('anonymous sessions are not allowed'));
+    } 
+    let session = await Session.findOne({ token }).populate('user');
+    if (!session) {
+      next(new Error('wrong or expired session token'));
+    }
+    socket.user = session.user;
     next();
   });
 
-  io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+  io.on('connection', (socket) => {
+    socket.on('message', async (msg) => {
+      let message = new Message({ 
+        date: new Date(),
+        text: msg,
+        chat: socket.user.id,
+        user: socket.user.displayName
+      });
+      await message.save();
+    });
   });
-
+  
   return io;
 }
 
